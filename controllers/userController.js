daf149ad9009812583a4e3c5cc54a3ec749788a2
RenaldKS <109418521+RenaldKS@users.noreply.github.com>
2023-04-18 00:40:56 +0700

    Fix
    Adjusted userController, and updating reflectionController use,  reworked on the routers index.js.
    For Now Available:
    Get All Reflection by User ID logged on;;
    
    Create New Reflections

--- controllers/userController.js
+++ controllers/userController.js
@@ -7,7 +7,9 @@ class UserController {
         try {
             const { email, password } = req.body;
             const hash = hashPassword(password);
-            const data = await connection.query('INSERT INTO users (email, password) VALUES ($1, $2) RETURNING *', [email, hash]);
+            const data = await connection.query('INSERT INTO users (email, password) VALUES ($1, $2) RETURNING *', [[email], [hash]]);
+            console.log(data.rows); // log the rows returned by the query
+            console.log(data.rows[0]); // log the first row of the result set   
             res.status(201).json(data.rows[0]);
         } catch (error) {
             console.log(error);
@@ -18,12 +20,12 @@ class UserController {
     static async login(req, res) {
         try {
             const { email, password } = req.body;
-            const data = await connection.query('SELECT * FROM users WHERE email = $1', [email]);
+            const data = await connection.query('SELECT * FROM users WHERE email = $1', [[email]]);
             if (data.rows.length === 0) {
                 res.status(404).json({message: 'User Not Found'});
             } else {
                 const user = data.rows[0];
-                const isValid = comparePassword(password, user.password);
+                const isValid = comparePassword(password, user.password[0]);
                 if (isValid) {
                     const token = generateToken({id: user.id, email: user.email});
                     res.status(200).json({token});
@@ -32,13 +34,16 @@ class UserController {
                 }
             }
         } catch (error) {
+            console.log(error)
             res.status(500).json(error);
         }
     }
 
     static async getuser(req, res) {
         try {
+            const loginid = req.user.id ;
             const data = await connection.query('SELECT * FROM users');
+            console.log(loginid)
             res.status(200).json(data.rows);
         } catch (error) {
             res.status(500).json(error);
--- routers/index.js
+++ routers/index.js
@@ -3,15 +3,15 @@ const router = express.Router();
 const userController = require('../controllers/userController');
 const authentication = require('../middlewares/authentication');
 // const authorization = require('../middlewares/authorization');
-//const ReflectionController = require('../controllers/reflectionController');
+const ReflectionController = require('../controllers/reflectionController');
 
 router.post('/api/v1/users/register', userController.register);
 router.post('/api/v1/users/login', userController.login);
 router.use(authentication);
 router.get('/api/v1/users', userController.getuser);
-// router.post('/api/v1/reflections', ReflectionController.CreateReflection);
-// router.get('/api/v1/reflections', ReflectionController.GetAllReflectionsbyId);
-// router.put('/api/v1/reflections/:id',ReflectionController.UpdateReflection);
-// router.delete('/api/v1/reflections/:id,',ReflectionController.DeleteReflection);
+router.post('/api/v1/reflections', ReflectionController.CreateReflection);
+router.get('/api/v1/reflections', ReflectionController.GetAllReflectionsbyId);
+router.put('/api/v1/reflections/:id',ReflectionController.UpdateReflection);
+router.delete('/api/v1/reflections/:id,',ReflectionController.DeleteReflection);
  
 module.exports = router;
